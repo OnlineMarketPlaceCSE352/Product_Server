0220119ce69e701e0a892161daa00f19e9ad82b1
do7amo7amed <dohahabeb15@gmail.com>
2026-04-22 03:20:13 +0200

    initial commit

---
+++ src/main/java/org/project/model/Product.java
@@ -0,0 +1,20 @@
+package org.project.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class Product {
+    private String id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private String sellerID;
+    private Boolean available;
+
+}
